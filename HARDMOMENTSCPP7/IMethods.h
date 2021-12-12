cd687ad56b007191dac1e0fa5017837dacc00535
h'ella <h'ella@HELLA>
2021-12-12 21:19:49 +0300

    Добавьте файлы проекта.

---
+++ HARDMOMENTSCPP7/IMethods.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include "classes.pb.h"
+
+class IMethods {
+    virtual double GetAverageScore(const foo::bar::FullName& name) = 0;
+    virtual std::string GetAllInfo(const foo::bar::FullName& name) = 0;
+    virtual std::string GetAllInfo() = 0;
+};
---
+++ HARDMOMENTSCPP7/IRepository.h
@@ -0,0 +1,7 @@
+
+#pragma once
+
+class IRepository {
+    virtual void Open(std::string filename) = 0; // бинарная десериализация в файл
+    virtual void Save() = 0; // бинарная сериализация в файл
+};
